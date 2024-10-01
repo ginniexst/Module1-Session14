89190885c3e376a5ee9d001089e3e2a367cd0f05
Ginnie <ginniexst@gmail.com>
2024-10-01 13:48:47 +0900

    Update bai7.js

--- bai7.js
+++ bai7.js
@@ -3,7 +3,6 @@ let addBtn = document.getElementById("addRow");
 
 addBtn.onclick = function() {
     let row = table.insertRow();
-    console.log(row);
     for (let i = 0; i < 3; i++) {
         let cell = row.insertCell(i);
         cell.innerHTML = `Cell ${i+1} row ${row.rowIndex+1}`
